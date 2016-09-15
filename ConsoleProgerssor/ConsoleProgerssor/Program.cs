e41f6a750967e1c0a181fe7558fb0f41ba63bbc0
antonvdovin <antonvdovin92@gmail.com>
2016-09-15 16:49:53 +0300

    initial project

---
+++ ConsoleProgerssor/ConsoleProgerssor/Program.cs
@@ -0,0 +1,26 @@
+﻿using System;
+using System.Threading;
+
+namespace ConsoleProgerssor
+{
+    public class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.Write("Performing some task... ");
+
+            const int totalProgressCount = 100;
+
+            using (var progress = new ProgressBar())
+            {
+                for (var i = 0; i <= totalProgressCount; i++)
+                {
+                    progress.Report((double)i / totalProgressCount);
+                    Thread.Sleep(50);
+                }
+            }
+            Console.WriteLine("Done.");
+            Console.ReadKey();
+        }
+    }
+}
